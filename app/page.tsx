33ac967b580bce89a9cc64a45c590b723d31df2a
unknown <kurkisen@gmail.com>
2024-01-28 21:54:15 +0200

    dev work

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Index from "@/containers/home-page";
+import { getAllProgrammingSkills } from "@/libs/api";
 
-export default function Home() {
+export default async function Home() {
+  const pageData = await getAllProgrammingSkills();
   return (
     <main>
-      <Index />
+      <Index pageData={pageData} />
     </main>
   );
 }
